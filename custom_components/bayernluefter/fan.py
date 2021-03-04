b41a244ff822fd054b7721660677259c086ec3b9
Niels Mündler <n.muendler@tum.de>
2021-03-04 22:20:17 +0100

    Update fan for new home assistant

--- custom_components/bayernluefter/fan.py
+++ custom_components/bayernluefter/fan.py
@@ -3,47 +3,17 @@ Connect to a Bayernluefter via it's web interface and read and write data
 Switch to control the power state
 """
 import logging
-import datetime
+from typing import Optional
 
 from pyernluefter import Bayernluefter
 
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME, CONF_ICON, CONF_RESOURCE
-from homeassistant.const import (
-    STATE_UNKNOWN,
-    STATE_ON, STATE_OFF)
-from homeassistant.util import Throttle
-from homeassistant.components.fan import FanEntity, ATTR_SPEED, SUPPORT_SET_SPEED, SPEED_HIGH, SPEED_MEDIUM, SPEED_LOW, SPEED_OFF
+from homeassistant.components.fan import FanEntity, SUPPORT_SET_SPEED
+from homeassistant.util.percentage import ranged_value_to_percentage, int_states_in_range, percentage_to_ranged_value
 
 _LOGGER = logging.getLogger(__name__)
 DEFAULT_NAME = "Bayernluefter"
-SPEEDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-SUPPORTED_SPEEDS = [
-    SPEED_OFF,
-    SPEED_LOW,
-    SPEED_MEDIUM,
-    SPEED_HIGH
-]
-SPEED_TO_BL = {
-    SPEED_OFF: 1,
-    SPEED_LOW: 2,
-    SPEED_MEDIUM: 4,
-    SPEED_HIGH: 9,
-}
-BL_TO_SPEED = {
-    1: SPEED_OFF,
-    2: SPEED_LOW,
-    3: SPEED_LOW,
-    4: SPEED_MEDIUM,
-    5: SPEED_MEDIUM,
-    6: SPEED_MEDIUM,
-    7: SPEED_MEDIUM,
-    8: SPEED_HIGH,
-    9: SPEED_HIGH,
-    10: SPEED_HIGH,
-}
+
+BAYERNLUEFTER_SPEED_RANGE = (1, 10)
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Bayernluefter component"""
@@ -76,34 +46,30 @@ class BayernluefterFan(FanEntity):
         return self._name
 
     @property
+    def speed_count(self):
+        return int_states_in_range(BAYERNLUEFTER_SPEED_RANGE)
+
+    def _current_speed(self):
+        return self._bayernluefter.raw_converted()["Speed_Out"]
+
+    @property
+    def percentage(self):
+        return ranged_value_to_percentage(BAYERNLUEFTER_SPEED_RANGE, self._current_speed())
+
+    @property
     def is_on(self):
         """Return true if device is on."""
-        # State logs whether the timer mode is active or not
-        try:
-            return self._bayernluefter.raw_converted()["Speed_Out"] > SPEED_TO_BL[SPEED_LOW]
-        except KeyError:
-            return STATE_UNKNOWN
- 
-    @property
-    def speed(self) -> str:
-        try:
-            return BL_TO_SPEED[self._bayernluefter.raw_converted()["Speed_Out"]]
-        except KeyError:
-            return STATE_UNKNOWN
+        return self._current_speed() > 0
 
-    async def async_set_speed(self, speed: str) -> None:
-        await self._bayernluefter.set_speed(SPEED_TO_BL[speed])
+    async def async_set_speed(self, percentage: int) -> None:
+        await self._bayernluefter.set_speed(int(percentage_to_ranged_value(BAYERNLUEFTER_SPEED_RANGE, percentage)))
 
     async def async_turn_off(self, **kwargs):
-        await self._bayernluefter.set_speed(SPEED_TO_BL[SPEED_OFF])
+        await self._bayernluefter.set_speed(0)
 
-    async def async_turn_on(self, **kwargs):
-        await self._bayernluefter.set_speed(SPEED_TO_BL[SPEED_MEDIUM])
+    async def async_turn_on(self, percentage: Optional[int] = None, **kwargs):
+        await self.async_set_speed(percentage if percentage is not None else 50)
 
-    @property
-    def speed_list(self) -> list:
-        return SUPPORTED_SPEEDS
-    
     @property
     def supported_features(self) -> int:
         return SUPPORT_SET_SPEED
